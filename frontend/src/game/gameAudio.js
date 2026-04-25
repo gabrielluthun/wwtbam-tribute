2f2f40e1d5f87f89a04e5c46382925834b651964
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-04-25 02:19:08 +0200

    feat(gameAudio.js): fix audio delay in ms between steps

--- frontend/src/game/gameAudio.js
+++ frontend/src/game/gameAudio.js
@@ -1,7 +1,7 @@
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /** Attend la fin du son (ou minMs si plus long), cappe à maxMs */
-export const waitForSound = (soundPromise, minMs = 0, maxMs = 8000) =>
+export const waitForSound = (soundPromise, minMs = 0, maxMs = 4000) =>
   Promise.race([
     Promise.all([Promise.resolve(soundPromise), sleep(minMs)]),
     sleep(maxMs),
